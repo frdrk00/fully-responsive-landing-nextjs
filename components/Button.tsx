1e426fefcf15a76c7d9891ddaf48124eb3b17814
frdrk00 <masterkooo@gmail.com>
2023-10-14 14:59:02 +0200

    Camp section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
   type: 'button' | 'submit'
   title: string
   icon?: string
-  variant: 'btn_dark_green'
+  variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text' | 'btn_white'
 }
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
@@ -14,7 +14,7 @@ const Button = ({ type, title, icon, variant }: ButtonProps) => {
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   )
 }
